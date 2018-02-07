34fc6a0ab29fb283704857c2b9d31772574b9dc0
Derta <derta.isyajora@gmail.com>
2018-02-07 17:20:36 +0700

    Fix bug example of CRON

--- app/User.php
+++ app/User.php
@@ -18,7 +18,7 @@ class User extends Authenticatable
     protected $table = 'users';
     
     protected $fillable = [
-        'name', 'email', 'password','phone'
+        'name', 'email', 'password','phone','role_id'
     ];
 
     /**
